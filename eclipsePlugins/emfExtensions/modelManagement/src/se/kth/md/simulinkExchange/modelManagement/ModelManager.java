be13fe23827a0e7bfee1e0100109e6de2fdd1d2b
alex <alex@schenkman.info>
2010-04-22 14:12:34 +0000

    Commented public functions.

--- eclipsePlugins/emfExtensions/modelManagement/src/se/kth/md/simulinkExchange/modelManagement/ModelManager.java
+++ eclipsePlugins/emfExtensions/modelManagement/src/se/kth/md/simulinkExchange/modelManagement/ModelManager.java
@@ -17,11 +17,10 @@ import org.apache.commons.io.FilenameUtils;
 import se.kth.md.simulinkExchange.modelManagement.exceptions.InvalidModelException;
 
 /**
- * Resumen corto aca.
- * This class can read from file, validate, and write to file
- * ecore models. It can be used in Java standalone mode, 
- * as it first registers the metamodel.
- * In order to write models, subclasses should define initMetaModel method.
+ * Reads, writes and validates models from memory and files.
+ * 
+ * It is mainly a convenience wrapper around EMF, to simplify these operations.
+ * It can be used in Java stand alone mode, i.e. outside of Eclipse. 
  * 
  * \author Alex Schenkman
  */
@@ -35,30 +34,35 @@ public abstract class ModelManager {
 	
 	protected abstract void initMetaModel();
 
-	/** 
-	 * Brief desc ends here. The rest is the detailed description.
-	 * or Am i wrong?
-	 * 
-	 */
-	public ModelManager() {
-		this( "unnamed.xmi" );
-	}
-	
-	/**
-	 * @param modelPath File path to a model file.
+    /**
+	 * Opens a model from file.
+	 * It relies on ModelManager(URI model).
+	 * @param modelPath File path to a model.
 	 */
 	public ModelManager(String modelPath) {
 		this( URI.createFileURI(modelPath) );
 	}
 	
+	/**
+	 * Opens a model from a URI.
+	 * @param model URI path to a model.
+	 */
 	public ModelManager(URI model) {
 		init( model );
 	}
 
+	/**
+	 * Loads a previously opened model into memory.
+	 * Models are opened with this class' constructor.
+	 * @throws InvalidModelException If the model does not complies with the registered metamodel.
+	 */
 	public void loadIt() throws InvalidModelException {	
 		resource = resourceSet.getResource(model, true);
 	}
 
+	/**
+	 * Saves the model in memory to a file.
+	 */
 	public void saveIt() throws IOException {
 		if ( resource == null ) {
 			assert( getTopElement() != null );
@@ -67,24 +71,39 @@ public abstract class ModelManager {
 		resource.save(null);
 	}
 	
+	/** 
+	 * Saves the model in memory to a file with the given name.
+	 * @param pathname File name where to save the model.
+	 * @throws IOException
+	 */
 	public void saveAs(String pathname) throws IOException {
 		URI modelURI = URI.createFileURI(pathname);
 		saveAs(modelURI);		
 	}
 
+	/** 
+	 * Saves the model in memory to a URI with the given name.
+	 * @param modelURI URI where to save the model.
+	 * @throws IOException
+	 */
 	public void saveAs(URI modelURI) throws IOException {
 		Resource aNewResource = createNewResource(modelURI);
 		setTopElementIn( aNewResource );
 		aNewResource.save(null);
 	}
 		
+	/**
+	 * Saves the model to a file, replacing the original extension.
+	 * Extensions are specific for each metamodel, and defined by subclasses.
+	 * @param originalPathName Original file name.
+	 * @throws IOException
+	 */
 	public void saveAsWithPropperExtension(String originalPathName) throws IOException {
 		String newFileName = replaceExtension(originalPathName, fileExtension); 
 		saveAs( newFileName );
 	}
 
-	//This should be private, but I wanted a test case.
-	public String replaceExtension(String pathName, String extension) {
+	private String replaceExtension(String pathName, String extension) {
 		return FilenameUtils.removeExtension(pathName) + '.' + extension;
 	}
 	
@@ -96,18 +115,30 @@ public abstract class ModelManager {
 		aResource.getContents().add( topElement );		
 	}
 
+	/**
+	 * Validates that the model in memory complies with the metamodel.
+	 * It uses ModelValidator to achieve this.
+	 * @throws InvalidModelException
+	 */
 	public void validateIt() throws InvalidModelException {
 		assert(resource != null);
 		ModelValidator aModelValidator = new ModelValidator( resource );
 		aModelValidator.doIt();
 	}
 
+	/**
+	 * @return the top element from the model.
+	 */
 	public EObject getTopElement() {
 		EObject result = ( resource == null )? 
 				null : resource.getContents().get(0);
 		return result;
 	}
 
+	/**
+	 * Sets topElement at the top if the model hierarchy.
+	 * @param topElement
+	 */
 	public void setTopElement(EObject topElement ) {
 		if ( resource == null ) {
 			resource = createNewResource( model );
@@ -119,16 +150,23 @@ public abstract class ModelManager {
 		return resourceSet.createResource( modelURI );
 	}
 
+	/**
+	 * @return the string that identifies this metamodel: NsURI.
+	 */
 	public String getNsURI() {
 		return ePackage.getNsURI();
 	}
 
+	/**
+	 * The factory is used to create elements from this metamodel.
+	 * @return the factory class for this metamodel.
+	 */
 	public EFactory getFactory() {
 		return ePackage.getEFactoryInstance();
 	}
 
 	/**
-	 * Register the appropriate resource factory to handle file extensions.
+	 * Registers the appropriate resource factory to handle file extensions.
 	 * In this case, all files (*) will handled by XMI resource Factory.
 	 */
 	private void setResourceFactoryForFileExtension(String extension) {
@@ -144,6 +182,9 @@ public abstract class ModelManager {
 		initMetaModel();
 	}
 
+	/**
+	 * @return the file extension used by this metamodel.
+	 */
 	public String getFileExtension() {
 		return fileExtension;
 	}
