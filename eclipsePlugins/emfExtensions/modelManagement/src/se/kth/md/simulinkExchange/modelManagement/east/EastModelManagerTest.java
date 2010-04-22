332c76ca70826ccbc2b51fb6382bf5aa00a05997
alex <alex@schenkman.info>
2010-04-22 10:07:29 +0000

    Added tests and dependencies: JUnit and JExample.

---
+++ eclipsePlugins/emfExtensions/modelManagement/src/se/kth/md/simulinkExchange/modelManagement/east/EastModelManagerTest.java
@@ -0,0 +1,53 @@
+package se.kth.md.simulinkExchange.modelManagement.east;
+
+//import se.kth.md.simulinkExchange.simulink.SimulinkModelManager;
+
+import org.eclipse.emf.common.util.URI;
+//import org.junit.Test;
+
+import se.kth.md.simulinkExchange.modelManagement.east.EastModelManager;
+import se.kth.md.simulinkExchange.modelManagement.exceptions.InvalidModelException;
+
+public class EastModelManagerTest {
+
+	private String modelPath 	= "../ATLtrafos/testFiles/MyEASTModel1.uml";
+	private URI modelURI 		= URI.createFileURI(modelPath);
+
+	public void testValid() throws InvalidModelException {
+		//SimulinkModelManager smm = new SimulinkModelManager( modelURI );
+		EastModelManager mm = new EastModelManager( modelURI );
+		mm.loadIt();
+		mm.validateIt();
+	}
+
+	public static void main(String [] args) throws InvalidModelException {
+		EastModelManagerTest mm = new EastModelManagerTest();
+		mm.testValid();
+	}
+	
+/*	@Test
+	public void testInitMetaModel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFactory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEastModelManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEastModelManagerString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEastModelManagerURI() {
+		fail("Not yet implemented");
+	}
+*/
+}
