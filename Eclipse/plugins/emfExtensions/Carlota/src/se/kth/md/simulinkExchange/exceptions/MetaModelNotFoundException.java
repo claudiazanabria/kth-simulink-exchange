b61d69e8ed72a03af356882b17e3c033c4875dd6
alex <alex@schenkman.info>
2010-05-06 15:06:11 +0000

    Restoring plugins that got deleted in rev.288.

---
+++ Eclipse/plugins/emfExtensions/Carlota/src/se/kth/md/simulinkExchange/exceptions/MetaModelNotFoundException.java
@@ -0,0 +1,10 @@
+package se.kth.md.simulinkExchange.exceptions;
+
+public class MetaModelNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
