aea27f1fa4dab5c47f978c82b5c1af2d03f6c2e5
alex <alex@schenkman.info>
2010-06-28 13:26:33 +0000

    Going from EAST to Simliunk: Moved naming of simulink ecore models from preprocessing to postprocessing. preprocessing == before matlab can use this. postprocessing = after the ATL transformation.

--- Eclipse/plugins/SimulinkExchange/src/se/kth/md/simulinkExchange/management/ModelManager.java
+++ Eclipse/plugins/SimulinkExchange/src/se/kth/md/simulinkExchange/management/ModelManager.java
@@ -191,4 +191,9 @@ public abstract class ModelManager {
 		this.fileExtension = fileExtension;
 	}
 
+	public void traverseWith(Traverser traverser) {		
+		traverser.doIt( this );
+		
+	}
+
 }
---
+++ Eclipse/plugins/SimulinkExchange/src/se/kth/md/simulinkExchange/management/Traverser.java
@@ -0,0 +1,6 @@
+package se.kth.md.simulinkExchange.management;
+
+
+public interface Traverser {
+	public void doIt(ModelManager manager);
+}
