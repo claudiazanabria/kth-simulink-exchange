4259d5c41a699ee10111f657a1b9c9adaf65ccca
carljohan.sjostedt <carljohan.sjostedt@gmail.com>
2010-06-29 13:09:22 +0000

    License headers updated

--- matlab/code/closeAll.m
+++ matlab/code/closeAll.m
@@ -1,4 +1,15 @@
+
 %% Close all systems
+%**************************************************************************
+%* Copyright (c) 2010 KTH, Mechatronics group and others.
+%* All rights reserved. This program and the accompanying materials
+%* are made available under the terms of the Eclipse Public License v1.0,
+%* which accompanies this distribution, and is available at
+%* http://www.eclipse.org/legal/epl-v10.html
+%* 
+%* Contributors: KTH, Mechatronics group
+%**************************************************************************
+
 sysList = find_system();
 for x=1:size(sysList)
     close_system( sysList(x), 0 );
