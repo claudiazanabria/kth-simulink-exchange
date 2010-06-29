f9c15f57e0baf766c5fff2cc046b799465e0ab80
alex <alex@schenkman.info>
2010-06-29 17:40:00 +0000

    Added EPL license text to all Java files.

--- Eclipse/plugins/SimulinkExchange/src/se/kth/md/simulinkExchange/Activator.java
+++ Eclipse/plugins/SimulinkExchange/src/se/kth/md/simulinkExchange/Activator.java
@@ -1,3 +1,13 @@
+/**************************************************************************
+ * Copyright (c) 2010 KTH, Mechatronics group and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0,
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: KTH, Mechatronics group
+ **************************************************************************/
+
 package se.kth.md.simulinkExchange;
 
 import java.io.FileNotFoundException;
