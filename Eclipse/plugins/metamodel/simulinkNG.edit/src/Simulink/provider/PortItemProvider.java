a4ca108f5085ded772d3e5e07f84b1199ac0cd82
alex <alex@schenkman.info>
2010-07-05 13:07:45 +0000

    Removed wrong EOpossite property from ports.

--- Eclipse/plugins/metamodel/simulinkNG.edit/src/Simulink/provider/PortItemProvider.java
+++ Eclipse/plugins/metamodel/simulinkNG.edit/src/Simulink/provider/PortItemProvider.java
@@ -9,14 +9,12 @@ package Simulink.provider;
 
 import Simulink.Port;
 
-import Simulink.SimulinkPackage;
 import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
 
-import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
