28ff4bfbed92fd482a36349461fe921e2e937e47
alex <alex@schenkman.info>
2010-08-25 10:00:22 +0000

    First attempt to automatic builds with Ant. Not complete.

---
+++ Eclipse/plugins/SimulinkExchange/unitTests/se/kth/md/simulinkExchange/management/AlexTest.java
@@ -0,0 +1,12 @@
+package se.kth.md.simulinkExchange.management;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.runner.RunWith;
+
+@RunWith(JMock.class)
+class AlexTest {
+    Mockery context = new JUnit4Mockery();
+}
