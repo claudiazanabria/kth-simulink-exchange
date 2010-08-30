58b14dd403f1d890b43e8e620df05dcb6ffb010e
alex <alex@schenkman.info>
2010-08-30 09:24:01 +0000

    Continue adding tests.

--- Eclipse/plugins/SimulinkExchange/src/se/kth/md/simulinkExchange/conversion/ToSimulink/postprocessing/NameTraverserDelegate.java
+++ Eclipse/plugins/SimulinkExchange/src/se/kth/md/simulinkExchange/conversion/ToSimulink/postprocessing/NameTraverserDelegate.java
@@ -4,11 +4,15 @@ import Simulink.Inport;
 import Simulink.Line;
 import Simulink.Model;
 import Simulink.Outport;
+import Simulink.Port;
 import Simulink.ProtoObject;
 import Simulink.System;
 import Simulink.SystemReference;
 
 public class NameTraverserDelegate implements ITraverserDelegate {
+
+	private static final String INPORT_SUFFIX = "_In";
+	private static final String OUTPORT_SUFFIX = "_Out";
 
 	@Override
 	public void doIt(SystemReference systemReference) {
@@ -29,23 +33,80 @@ public class NameTraverserDelegate implements ITraverserDelegate {
 	}
 
 	@Override
-	public void doIt(Inport with) {
-		// TODO Auto-generated method stub
+	public void doIt(Inport inport) {
+		addTypeSuffixIfNeeded( inport, INPORT_SUFFIX );
+		computePortName(inport);	
+	}
 
+	private void computePortName(Port port) {
+		String parentName = port.getParent().getSimulinkName();
+		computeEnclosingSimulinkName(port, parentName);
 	}
 
 	@Override
-	public void doIt(Outport with) {
-		// TODO Auto-generated method stub
-
+	public void doIt(Outport outport) {
+		addTypeSuffixIfNeeded( outport, OUTPORT_SUFFIX );
+		computePortName(outport);	
 	}
 
 	@Override
-	public void doIt(Line with) {
-		// TODO Auto-generated method stub
-
+	public void doIt(Line line) {
+		String simulinkName = null;
+		simulinkName = processPort(line, line.getSource());
+		line.setSimuNameSrc(simulinkName);
+		simulinkName = processPort(line, line.getDestination());
+		line.setSimuNameDst(simulinkName);
 	}
 
+	protected String processPort(Line aLine, Port port) {
+		String simuName = null;
+		if ( lineAndPortHaveTheSameParent(aLine, port) ) {
+			simuName = port.getName() + "/1";  
+		} else {
+			System lineParent = aLine.getParent();
+			SystemReference sysRef = findSysRefWhosTargetIsGivenSystem( lineParent, port.getParent() );
+			simuName = sysRef.getName() + '/' + findPositionWithinSystem(sysRef.getTarget(), port); 			
+		}
+		return simuName;
+	}
+	
+
+	/**
+	 * Returns the position in the Inports or Ouports array for the given port
+	 * @param aSystem
+	 * @param port
+	 * @return 0 if port is not found
+	 */
+	protected Integer findPositionWithinSystem(System aSystem, Port port) {
+		int index = aSystem.getInports().indexOf(port);
+		if ( index == -1 ) {
+			index = aSystem.getOutports().indexOf(port);
+		}		
+		return index+1;
+	}
+
+	protected boolean lineAndPortHaveTheSameParent(Line aLine, Port port) {
+		return aLine.getParent() == port.getParent();	
+	}
+	
+
+	// TODO test
+	protected SystemReference findSysRefWhosTargetIsGivenSystem(System lineParent, System portsParent) {
+		for (SystemReference sysRef : lineParent.getChildren()) {
+			if ( sysRef.getTarget() == portsParent ) {
+				return sysRef; 
+				}
+		}
+		return null;
+	}
+	
+	protected void addTypeSuffixIfNeeded(Port port, String suffix) {
+		String name = port.getName(); 
+		if ( ! name.endsWith(suffix) ) {
+			port.setName( name + suffix );
+		}				
+	}
+	
 	protected void computeGenericSimulinkName(ProtoObject pObject) {
 		pObject.setSimulinkName( pObject.getName() );
 	}
--- Eclipse/plugins/SimulinkExchange/unitTests/se/kth/md/simulinkExchange/conversion/ToSimulink/postprocessing/NameTraverserDelegateTest.java
+++ Eclipse/plugins/SimulinkExchange/unitTests/se/kth/md/simulinkExchange/conversion/ToSimulink/postprocessing/NameTraverserDelegateTest.java
@@ -1,34 +1,26 @@
 package se.kth.md.simulinkExchange.conversion.ToSimulink.postprocessing;
 
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
-import org.jmock.Mockery;
-import org.jmock.Sequence;
-import org.jmock.integration.junit4.JMock;
-import org.jmock.integration.junit4.JUnit4Mockery;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import se.kth.md.simulinkExchange.management.simulink.SimulinkModelManager;
-
 import Simulink.Inport;
-import Simulink.Line;
 import Simulink.Model;
 import Simulink.Outport;
+import Simulink.Port;
 import Simulink.SimulinkFactory;
-import Simulink.SimulinkPackage;
 import Simulink.System;
-import Simulink.SystemReference;
 
-@RunWith(JMock.class)
 public class NameTraverserDelegateTest {
 
 	private static final String NAME = "Charly";
 	private static final String FILENAME = "Charly.mdl";
 	
-	Mockery context 		= new JUnit4Mockery();
+/*	Mockery context 		= new JUnit4Mockery();
 	Model	modelMock	= context.mock(Model.class);
 	System  systemMock  = context.mock(System.class);
 	SystemReference systemReferenceMock = context.mock(SystemReference.class);
@@ -37,13 +29,15 @@ public class NameTraverserDelegateTest {
 	Line lineMock = context.mock(Line.class);
 	
 	Sequence aSequence 	= context.sequence("aSequence");
-
+*/
 	SimulinkModelManager manager = new SimulinkModelManager();
 	SimulinkFactory factory = manager.getFactory();
 
 	NameTraverserDelegate traverser;
 	Model theModel;
 	System rootSystem;
+	Inport inport;
+	Outport outport;
 
 	
 	@Before
@@ -52,6 +46,9 @@ public class NameTraverserDelegateTest {
 		theModel = factory.createModel();
 		theModel.setName(NAME);
 		rootSystem = factory.createSystem();
+		inport 	= factory.createInport();
+		outport = factory.createOutport();
+
 	}
 	
 	
@@ -61,7 +58,7 @@ public class NameTraverserDelegateTest {
 		rootSystem.setName("this should be overwritten");
 		theModel.setRoot(rootSystem);
 
-		traverser.setRootSystemNameSameAsModel(modelMock);
+		traverser.setRootSystemNameSameAsModel(theModel);
 		assertEquals(rootSystem.getName(),NAME);
 	}
 	
@@ -77,10 +74,58 @@ public class NameTraverserDelegateTest {
 		rootSystem.setName(NAME);
 		assertFalse(FILENAME.equals(rootSystem.getFilename()));
 		traverser.computeSystemFileName(rootSystem);
-		assertEquals(rootSystem.getFilename(),FILENAME);
-		assertFalse(rootSystem.getSimulinkName().equals(FILENAME));
+		assertEquals(FILENAME,rootSystem.getFilename());
+		assertFalse(FILENAME.equals(rootSystem.getSimulinkName()));
 	}
 	
-	//TODO computeEnclosingSimulinkNameTest
+	@Test
+	public void enclosingSimulinkNameTest() {
+		final String simulinkName = NAME + '/' + rootSystem.getName();
+		traverser.computeEnclosingSimulinkName(rootSystem, NAME);
+		assertEquals(rootSystem.getSimulinkName(), simulinkName);
+	}
+	
+	@Test
+	public void addTypeSuffixBecauseMissingTest() {
+		final String suffix = "something";
+		inport = factory.createInport();
+		inport.setName(NAME);
+		traverser.addTypeSuffixIfNeeded(inport, suffix);
+		assertEquals(NAME+suffix, inport.getName());
+	}
+
+	@Test
+	public void dontAddTypeSuffixBecausePresentTest() {
+		final String suffix = "something";
+		inport = factory.createInport();
+		inport.setName(NAME+suffix);
+		traverser.addTypeSuffixIfNeeded(inport, suffix);
+		assertEquals(NAME+suffix, inport.getName());
+	}
+
+	@Test
+	public void getParentOnPortHackTest() {
+		rootSystem.getInports().add(inport);
+		rootSystem.getOutports().add(outport);
+		Port port = inport;
+		assertEquals(rootSystem, port.getParent());
+		port = outport;
+		assertEquals(rootSystem, port.getParent());
+	}
+	
+	@Test
+	public void findPositionWithinSystemTest() {
+		rootSystem.getInports().add(inport);
+		rootSystem.getOutports().add(outport);
+		Inport inport2 = factory.createInport();
+		rootSystem.getInports().add( inport2 );
+		assertEquals(new Integer(2),traverser.findPositionWithinSystem(rootSystem, inport2));
+		assertEquals(new Integer(1),traverser.findPositionWithinSystem(rootSystem, outport));
+	}
+	
+	@Test
+	public void positionWithinSystemNotFoundShouldReturnZeroTest() {
+		assertEquals(new Integer(0),traverser.findPositionWithinSystem(rootSystem, inport));
+	}
 
 }
