bd408c62a6c1ea6a2eb07af8feda1100e4b22f82
carljohan.sjostedt <carljohan.sjostedt@gmail.com>
2010-09-27 11:43:19 +0000

    WIP - not working

--- matlab/SimulinkAPI/tests/TestModel.m
+++ matlab/SimulinkAPI/tests/TestModel.m
@@ -1,11 +1,22 @@
 classdef TestModel < TestCase
 
     properties
-        t
+        aModel;
+        refBloc;
+        Block;
+        GainBlock;
+        RF1OutportHandle;
+        F2Inport;
+        line1;
     end
     
     properties(Constant)
-        modelName = 'systemundertest';
+        % Use consistent case
+        modelName = 'yorkDummy';
+        RF1Name = 'RF1';
+        F2Name = 'F2';
+        wrongName = 'I do not exists';
+        line1UUID
     end
     
     methods
@@ -15,15 +26,64 @@ classdef TestModel < TestCase
 
         function setUp(self)
             new_system(self.modelName);
-            self.t = Model(self.modelName);
+            open_system(self.modelName);
+            add_block('built-in/subsystem', [self.modelName '/Subsystem']);
+            add_block('built-in/Inport', [self.modelName '/Subsystem/Inport1']);
+            add_block('built-in/Outport', [self.modelName '/Subsystem/Outport1']);
+            
+            add_block('built-in/subsystem', [self.modelName '/Subsystem1']);
+            add_block('built-in/Inport', [self.modelName '/Subsystem1/Inport1']);
+            add_block('built-in/Outport', [self.modelName '/Subsystem1/Outport1']);
+            add_line(self.modelName,'Subsystem/1','Subsystem1/1')
+            add_line(self.modelName,'Subsystem1/1','Subsystem/1')
+            add_block('built-in/subsystem', [self.modelName '/Subsystem2']);
+            add_block('built-in/Inport', [self.modelName '/Subsystem2/Inport1']);
+            add_block('built-in/Outport', [self.modelName '/Subsystem2/Outport1']);
+            add_block('built-in/subsystem', [self.modelName '/Subsystem2/Subsystem3']);
+            add_block('built-in/Inport', [self.modelName '/Subsystem2/Subsystem3/Inport1']);
+            add_block('built-in/Outport', [self.modelName '/Subsystem2/Subsystem3/Outport1']);
+            self.RF1OutportHandle = 2323; % ports have two handles!?!
+            self.aModel = Model.recreateFromModel(self.modelName);
         end
 
         function tearDown(self)
             close_system(self.modelName,0);
         end
 
+        function testGetChildren(self)
+            children = self.aModel.children;
+            assertEqual(3,children.size );            
+            assertEqual(1,aModel.numberOfChildrenOfType( GainBlock.class ) );
+            assertEqual(1,aModel.numberOfChildrenOfType( Block.class ) );
+            assertEqual(1,aModel.numberOfChildrenOfType( refBlock.class ) );
+        end
+        
+        function testModelContainmentWithName( self )
+            assertTrue( aModel.contains( self.F2Name ) );
+            assertFalse( aModel.contains( self.wrongName ));
+            block = aModel.child( self.F2Name );
+            assertEquals(self.F2UUID, block.UUID());
+        end
+        
+        function testModelContainmentWithUUID( self )
+            assertTrue( aModel.contains( self.F2UUID ) );
+            assertFalse( aModel.contains( self.UUID ));
+            block = aModel.child( self.F2UUID );
+            assertEquals(self.F2Name, block.name());            
+        end
+        
+        function testLines(self)
+            line1 = self.aModel.lines( self.line1UUID );
+            assertEqual(self.RF1OutportHandle, line1.source.handle);
+            
+
+        end
+        
+        
         function testModelHandle(self)
             assertEqual(get_param(self.modelName,'handle'),self.t.handle)
+            
+            
         end
         
         function testUnknownModel(self)
