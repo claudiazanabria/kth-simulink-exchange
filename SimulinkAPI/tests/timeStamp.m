e64b84bf484eb5938b92d4e7f0ac337ada9642a0
alex <alex@schenkman.info>
2010-10-04 08:44:59 +0000

    moved some files within a package

--- SimulinkAPI/tests/timeStamp.m
+++
@@ -1,7 +0,0 @@
-%TIMESTAMP Creates a time-stamped message
-%  example: 
-function string = timeStamp( message )
-    dateStringNow = datestr(now,'dd-mmm-yy HH:MM:SS');
-    string = [message ' @' dateStringNow];
-end
-
