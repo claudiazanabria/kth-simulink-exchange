8506198a164c734551cb608220372c100706e9ab
alex <alex@schenkman.info>
2010-10-12 08:13:52 +0000

    added source folder for tests

---
+++ SimulinkMetamodelOOAPI/Metamodel/SimulinkOOAPI/tests/src/SimulinkOOAPI/impl/ChildrenSupportImplTest.java
@@ -0,0 +1,23 @@
+package SimulinkOOAPI.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ChildrenSupportImplTest {
+	
+	ChildrenSupportImpl childrenSupport;
+	
+	@Before
+	public void setUp(){
+		childrenSupport = new ChildrenSupportImpl();
+	}
+	
+	@Test
+	public void testGetChildrenOfTypeGain(){
+		
+	}
+
+}
